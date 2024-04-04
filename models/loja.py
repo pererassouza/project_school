c00fbd2b9fd5191992f4658d92ec7a26eb674aa1
Felipe Souza <pererassouza@gmail.com>
2024-04-04 17:55:44 -0300

    Adicionado database

--- models/loja.py
+++ models/loja.py
@@ -1,45 +1,75 @@
 from produto import Produto
+import sqlite3
 
 
 class Loja:
-    def __init__(self, nome: str):
+    def __init__(self, nome):
         self.nome = nome
-        self.produtos = {}
+        self.conn = sqlite3.connect('databases/loja.db')
+        self.criar_tabela()
 
-    def adicionar_produto(self, produto: Produto, quantidade_inicial: int)\
-            -> None:
-        self.produtos[produto.nome] = produto
-        produto.atualizar_quantidade(quantidade_inicial)
+    def criar_tabela(self):
+        cursor = self.conn.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS produtos
+                          (id INTEGER PRIMARY KEY, nome TEXT, preco REAL, \
+                       quantidade INTEGER)''')
+        self.conn.commit()
 
-    def vender_produto(self, nome_produto: str, quantidade: int) -> None:
-        produto = self.produtos.get(nome_produto)
-        if produto and produto.quantidade >= quantidade:
-            produto.atualizar_quantidade(-quantidade)
-            print(f"{quantidade} unidade de {nome_produto} foram vendidas.")
+    def adicionar_produto(self, produto):
+        cursor = self.conn.cursor()
+        cursor.execute("INSERT INTO produtos (nome, preco, quantidade) VALUES \
+                       (?, ?, ?)",
+                       (produto.nome, produto.preco, produto.quantidade))
+        self.conn.commit()
+
+    def atualizar_produto(self, nome_produto, novo_preco, nova_quantidade):
+        cursor = self.conn.cursor()
+        cursor.execute("UPDATE produtos SET preco=?,\
+                        quantidade=? WHERE nome=?",
+                       (novo_preco, nova_quantidade, nome_produto))
+        self.conn.commit()
+        print(f"{nome_produto} atualizado com sucesso.")
+
+    def vender_produto(self, nome_produto, quantidade):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT * FROM produtos WHERE nome=?", (nome_produto,))
+        produto = cursor.fetchone()
+        if produto and produto[3] >= quantidade:
+            novo_estoque = produto[3] - quantidade
+            cursor.execute(
+                "UPDATE produtos SET quantidade=? WHERE nome=?",
+                (novo_estoque, nome_produto))
+            self.conn.commit()
+            print(f"{quantidade} unidades de {nome_produto} vendidas.")
         else:
             print("Produto não disponível em estoque.")
 
-    def verificar_estoque(self) -> None:
-        print("Estoque disponivel da loja:")
-        for produto in self.produtos.values():
-            print(f"{produto.nome}: {produto.quantidade} unidades")
+    def verificar_estoque(self):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT * FROM produtos")
+        produtos = cursor.fetchall()
+        print("Estoque disponível na loja:")
+        for produto in produtos:
+            print(f"{produto[1]}: {produto[3]} unidades")
 
 
+# Exemplo de uso
 if __name__ == "__main__":
-    # Criando alguns produtos
-    produto1 = Produto("Camiseta", 29.99, 50)
-    produto2 = Produto("Calça Jeans", 59.99, 30)
-    produto3 = Produto("Tênis", 79.99, 20)
-
     # Criando a loja
     loja = Loja("Minha Loja")
 
     # Adicionando produtos à loja
-    loja.adicionar_produto(produto1, 50)
-    loja.adicionar_produto(produto2, 30)
-    loja.adicionar_produto(produto3, 20)
+    loja.adicionar_produto(Produto(None, "Camiseta", 29.99, 50))
+    loja.adicionar_produto(Produto(None, "Calça Jeans", 59.99, 30))
+    loja.adicionar_produto(Produto(None, "Tênis", 79.99, 20))
 
     # Verificando estoque inicial
+    loja.verificar_estoque()
+
+    # Atualizando preço e estoque de um produto
+    loja.atualizar_produto("Camiseta", 80.20, 32)
+
+    # Verificando estoque após atualização
     loja.verificar_estoque()
 
     # Vendendo alguns produtos
--- models/produto.py
+++ models/produto.py
@@ -1,5 +1,6 @@
 class Produto:
-    def __init__(self, nome, preco, quantidade):
+    def __init__(self, id, nome, preco, quantidade):
+        self.id = id
         self.nome = nome
         self.preco = preco
         self.quantidade = quantidade
